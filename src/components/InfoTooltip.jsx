96e7a72e9ce5d8609babc2b5010848d5b4f0c54a
Daria Rutts <darya.rutts@yandex.ru>
2023-06-18 15:27:42 +0300

    style: исправить форматирование

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -7,16 +7,20 @@ function InfoTooltip({ isOpen, onClose, onOverlay, isSuccess }) {
     <div
     className={`popup ${isOpen ? 'popup_opened' : ''}`}
     onMouseDown={onOverlay}
-  >
+    >
     <div className="popup__container">
       <button
         className="button button_action_close"
         type="button"
         onClick={onClose}/>
       <div className="popup__info">
-        {isSuccess ? <img src={successIcon} alt="Черный крест в черном круге" className="popup__info-icon" /> : <img src={failureIcon} alt="Красный крест в красном круге" className="popup__info-icon" /> }
+        {isSuccess
+        ? <img src={successIcon} alt="Черный крест в черном круге" className="popup__info-icon" />
+        : <img src={failureIcon} alt="Красный крест в красном круге" className="popup__info-icon" /> }
         <div className="popup__info-container">
-          {isSuccess ? <p className="popup__info-text">Вы успешно зарегистрировались!</p> : <p className="popup__info-text">Что-то пошло не так! Попробуйте ещё раз.</p>}
+          {isSuccess
+          ? <p className="popup__info-text">Вы успешно зарегистрировались!</p>
+          : <p className="popup__info-text">Что-то пошло не так! Попробуйте ещё раз.</p>}
         </div>
       </div>
     </div>
